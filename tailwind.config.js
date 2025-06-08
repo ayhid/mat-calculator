bf5491a192cbdc9e25018592064e9ff4d5733297
Ayoub Hidri <ayhid@users.noreply.github.com>
2025-06-08 14:37:07 +0200

    upgrade to tailwind 4

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/**/*.{js,ts,jsx,tsx,mdx}'
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './styles/**/*.{css,scss,sass,less,styl}'
   ],
   theme: {
     extend: {
