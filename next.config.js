fa56060182faef389f198d31fca86a3fd82f4b47
Ayoub Hidri <ayhid@users.noreply.github.com>
2025-06-08 14:27:07 +0200

    Remove unused config and update globals.css

--- next.config.js
+++ next.config.js
@@ -1,22 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    appDir: true
-  },
-  images: {
-    unoptimized: true
-  },
-  trailingSlash: true,
-  output: 'export',
-  distDir: 'out',
-  basePath: '',
-  assetPrefix: '',
-  env: {
-    CUSTOM_KEY: 'mat-calculator-app'
+  
+  
   }
-}
 
 module.exports = nextConfig
 
